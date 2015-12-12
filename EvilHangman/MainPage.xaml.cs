4aae07a1416f12a7070d611cae111647091f0db1
Karl <karl.valliste@gmail.com>
2015-12-12 05:36:37 +0200

    hakkab looma

--- EvilHangman/MainPage.xaml.cs
+++ EvilHangman/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,10 +28,26 @@ namespace EvilHangman
 
         Dictionary<int, List<string>> words = new Dictionary<int, List<string>>();
         List<string> remaining;
+        public string[] result;
         public MainPage()
         {
             this.InitializeComponent();
             Setup();
+            SizeEntered();
+            ProposedLetter("a");
+            ProposedLetter("e");
+            ProposedLetter("i");
+            ProposedLetter("p");
+            ProposedLetter("k");
+            ProposedLetter("l");
+            ProposedLetter("t");
+            ProposedLetter("m");
+            ProposedLetter("u");
+            ProposedLetter("n");
+            foreach (string eunuhh in remaining)
+            {
+                Debug.WriteLine(eunuhh);
+            }
 
         }
 
@@ -64,11 +81,16 @@ namespace EvilHangman
         public void SizeEntered()
         {
             int size = 8;
+            result = new string[size];
             this.remaining = words[size];
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = "_";
+            }
         }
-        public bool ProposedLetter()
+        public bool ProposedLetter(string taht)
         {
-            string letter = "a";
+            string letter = taht;
             List<string> tempRemaining = new List<string>(); 
             foreach (string item in this.remaining)
             {
@@ -80,15 +102,64 @@ namespace EvilHangman
             if (tempRemaining.Count != 0)
             {
                 this.remaining = tempRemaining;
+                Debug.WriteLine(this.remaining.Count);
                 return false;
             }
             else
             {
-                //TODO erandi meetodid
-
+                int min = 100;
+                Dictionary<int, List<string>> occurances = new Dictionary<int, List<string>>();
                 foreach (string item in this.remaining)
                 {
-
+                    int a = Regex.Matches(item, letter).Count;
+                    if (a < min)
+                    {
+                        min = a;
+                    }
+                }
+                foreach (string item in this.remaining)
+                {
+                    if (min == 1)
+                    {
+                        if (Regex.Matches(item, letter).Count == min)
+                        {
+                            if (occurances.ContainsKey(item.IndexOf(letter)))
+                            {
+                                occurances[item.IndexOf(letter)].Add(item);
+                            }
+                            else
+                            {
+                                List<string> tempRemaining1 = new List<string>();
+                                tempRemaining1.Add(item);
+                            }
+                        }
+                    }
+                }
+                int max = 0;
+                int index = 0;
+                foreach (KeyValuePair<int, List<string>> entry in occurances)
+                {
+                    if (entry.Value.Count > max)
+                    {
+                        max = entry.Value.Count;
+                        index = entry.Key;
+                    }
+                }
+                //TODO siin on erind kinni püüdmata
+                remaining = occurances[index];
+                Debug.WriteLine(remaining.Count);
+                string asi = remaining[0];
+                for (int i = 0; i < asi.Length; i++)
+                {
+                    if (asi[i].Equals(letter))
+                    {
+                        this.result[i] = letter;
+                    }
+                }
+                foreach (string elem in result)
+                {
+                    Debug.Write(elem);
+                    Debug.WriteLine("");
                 }
                 return true;
             }
